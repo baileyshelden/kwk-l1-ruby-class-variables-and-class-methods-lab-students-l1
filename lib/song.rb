ea4e79f4c5072adca38d6f5e72025676cab9a654
baileyshelden <bailey7748@gmail.com>
2018-06-11 15:01:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,21 @@
-Class
+Class songs
+
+ def initialize(name, artist, genre)
+  @name = name
+  @artist = artist 
+  @genre = genre 
+ end 
+
+ def name 
+  @name 
+ end 
+
+ def artist 
+  @artist 
+ end 
+
+def genre 
+  @genre
+end 
+
+end 
